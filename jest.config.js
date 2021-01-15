369b3ddb72ee56c6f790ae4686a2d9632a46d42a
stevenyang <stevenyang@cn.ibm.com>
2021-01-15 09:02:51 +0800

    updated.

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
-const { defaults } = require('jest-config');
 module.exports = {
 	transform: {
-		".(ts|tsx)": 'ts-jest'
+		'.(ts|tsx)': 'ts-jest',
 	},
-	testMatch: ["<rootDir>/tests/**/*.(test|spec).(js|ts)"],
+	testMatch: ['<rootDir>/tests/**/*.(test|spec).(js|ts)'],
 	moduleFileExtensions: ['ts', 'js'],
 };
